978a30abdf0e0676bd6c7fc17a2d71b1a7f0fcb4
Minh Hoang Trinh <minhhoang.trinh@datadoghq.com>
2025-06-25 11:30:26 +0200

    initial setup

---
+++ install.sh
@@ -0,0 +1,25 @@
+set -euo pipefail
+
+
+# Install Homebrew and setup workspace
+if ! command -v brew &>/dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+brew install derailed/k9s/k9s
+brew install htop
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+/bin/bash -c setup-workspace
+
+# Setup dotfiles
+DOTFILES_PATH="$HOME/dotfiles"
+# Symlink dotfiles to the root within your workspace
+find $DOTFILES_PATH -type f -path "$DOTFILES_PATH/.*" |
+while read df; do
+  link=${df/$DOTFILES_PATH/$HOME}
+  mkdir -p "$(dirname "$link")"
+  ln -sf "$df" "$link"
+done
