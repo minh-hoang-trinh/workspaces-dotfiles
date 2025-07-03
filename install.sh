7abfd5b9a0f4ffb28bec46cad77b5ad66c214875
Minh Hoang Trinh <minhhoang.trinh@datadoghq.com>
2025-07-03 12:54:12 +0200

    update script

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# # Install Homebrew and setup workspace
-# if ! command -v brew &>/dev/null; then
-#   echo "Installing Homebrew..."
-#   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-#   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-# fi
-# brew install derailed/k9s/k9s
-# brew install htop
-# brew install zsh-autosuggestions
-# brew install zsh-syntax-highlighting
+if ! command -v brew &>/dev/null; then
+  echo "Installing Homebrew..."
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+/home/linuxbrew/.linuxbrew/bin/brew install derailed/k9s/k9s
+/home/linuxbrew/.linuxbrew/bin/brew install htop
+/home/linuxbrew/.linuxbrew/bin/brew install zsh-autosuggestions
+/home/linuxbrew/.linuxbrew/bin/brew install zsh-syntax-highlighting
 
 # Setup dotfiles
 DOTFILES_PATH="$HOME/dotfiles"
